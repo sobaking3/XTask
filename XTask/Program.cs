e3c90002a1ad704224602731910d17fcfb5f0a34
Мурад Рагимов <ragimovu1@tk34.mskobr.ru>
2022-04-13 12:51:37 +0300

    Добавьте файлы проекта.

---
+++ XTask/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace XTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
