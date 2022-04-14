77fc4598ea6ee10d7227d81bc3a562e4158ee5e3
Мурад Рагимов <ragimovu1@tk34.mskobr.ru>
2022-04-14 10:23:52 +0300

    Сделал 19, 20 задания

---
+++ XTask16/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace XTask16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 19. Создать метод который выводит на консоль сумму всех элементов массива кроме последнего
+            int[] ar = new int[5];
+            Random rnd = new Random();
+            int sum = 0;
+            for (int i = 0; i < ar.Length - 1; i++)
+            {
+                ar[i] = rnd.Next(1, 10);
+                sum += ar[i];
+                Console.Write(ar[i] + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine(sum);
+            Console.ReadKey();
+
+
+            // 20. Создать метод который считает произведение 5 случайных числе
+            int[] br = new int[5];
+            Random bnd = new Random();
+            int pr = 1;
+            for (int i = 0; i < ar.Length; i++)
+            {
+                ar[i] = rnd.Next(1, 10);
+                pr *= ar[i];
+                Console.Write(ar[i] + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine(pr);
+            Console.ReadLine();
+        }
+    }
+}
